c79bf080d4f3217b3db49a117b66c0ada68902b8
Développeur web <51367556+Francois-Legrand@users.noreply.github.com>
2021-06-15 14:24:40 +0200

    add controllerCategory

--- src/main/java/com/example/magasin/controllers/CategoryController.java
+++ src/main/java/com/example/magasin/controllers/CategoryController.java
@@ -1,12 +1,15 @@
 package com.example.magasin.controllers;
 
 import com.example.magasin.dto.CategoryDto;
-import com.example.magasin.models.Categories;
 import com.example.magasin.services.impl.CategoryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
 @RequestMapping("categories")
+@CrossOrigin
 public class CategoryController {
     @Autowired
     private CategoryServiceImpl service;
@@ -15,5 +18,16 @@ public class CategoryController {
     public CategoryDto save(@RequestBody CategoryDto categoryDto){
         return this.service.save(categoryDto);
     }
-
+    @GetMapping
+    public List<CategoryDto> findAll(){
+        return this.service.findAll();
+    }
+    @GetMapping("{id}")
+    public CategoryDto findById(@PathVariable Long id){
+        return this.service.findById(id);
+    }
+    @PutMapping
+    public CategoryDto edit(@RequestBody CategoryDto categoryDto){
+        return this.service.save(categoryDto);
+    }
 }
